926b3f68e79708176b7d63808947cdf92c43ee80
Murat Candaş Bozyiğit <bozyigit31@gmail.com>
2025-03-19 20:22:54 +0300

    d

---
+++ PortfolioAsp.Net/DAL/Entities/Features.cs
@@ -0,0 +1,9 @@
+﻿namespace PortfolioAsp.Net.DAL.Entities
+{
+    public class Features
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ PortfolioAsp.Net/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortfolioAsp.Net.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
